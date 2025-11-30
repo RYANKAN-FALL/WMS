4de0d0de65049756e77e947156bc9151d500a571
RYANKAN-FALL <prayogarkan12@gmail.com>
2025-12-01 00:50:10 +0700

    fix: add prisma client type alias

--- types/prisma.d.ts
+++ types/prisma.d.ts
@@ -1,1 +1,5 @@
-declare module "@prisma/client";
+declare module "@prisma/client" {
+  import { PrismaClient as PC } from "@prisma/client/runtime/library";
+  export const PrismaClient: typeof PC;
+  export type PrismaClient = PC;
+}
